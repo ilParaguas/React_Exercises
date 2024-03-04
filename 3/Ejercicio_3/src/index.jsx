e93e0aa96b1e3c2b9b542a675130788613c28e62
Francisco Javier Martinez Rodriguez <javic9165@gmail.com>
2024-03-04 20:34:56 +0100

    Fix again

--- 3/Ejercicio_3/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import { App } from "./app";
-import { createRoot } from "react-dom/client";
-
-const app = <App />;
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(app);
