c86fbcfc5701b3f7ac9ad3bf1880bafc09178fd9
Francisco Javier Martinez Rodriguez <javic9165@gmail.com>
2024-03-08 05:08:54 +0100

    Ejercicio 8

--- 8/Ejercicio_8/src/welcome.jsx
+++ 8/Ejercicio_8/src/welcome.jsx
@@ -5,6 +5,12 @@ export function Welcome(prop) {
     <div>
       <strong> Welcome, {prop.name}!</strong>;
       <Age age={prop.age} />
+      {prop.age > 18 && <Age age={prop.age} />}
+      {prop.age && <Age age={prop.age} />}
+      {prop.age > 18 && prop.age < 65 && <Age age={prop.age} />}
+      {prop.age > 18 && prop.age < 65 && prop.name === "John" && (
+        <Age age={prop.age} />
+      )}
     </div>
   );
 }
