b2cfa689efef817d341a4995f85dd77e6695eeb6
Francisco Javier Martinez Rodriguez <javic9165@gmail.com>
2024-03-10 22:19:31 +0100

    Semicolon fix

--- 8/Ejercicio_8/src/welcome.jsx
+++ 8/Ejercicio_8/src/welcome.jsx
@@ -3,7 +3,7 @@ import { Age } from "./age";
 export function Welcome(prop) {
   return (
     <div>
-      <strong> Welcome, {prop.name}!</strong>;
+      <strong> Welcome, {prop.name}!</strong>
       <Age age={prop.age} />
       {prop.age > 18 && <Age age={prop.age} />}
       {prop.age && <Age age={prop.age} />}
