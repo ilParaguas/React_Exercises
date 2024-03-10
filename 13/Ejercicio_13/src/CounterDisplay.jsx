c584ecb0e4c5c416139febb8c2d899aabf0a45bc
Francisco Javier Martinez Rodriguez <javic9165@gmail.com>
2024-03-10 01:47:58 +0100

    Ejercicio 14

--- 13/Ejercicio_13/src/CounterDisplay.jsx
+++
@@ -1,7 +0,0 @@
-export function CounterDisplay(prop) {
-  return (
-    <div>
-      <h2>Counter: {prop.count}</h2>
-    </div>
-  );
-}
--- 14/Ejercicio_14/src/App.jsx
+++ 14/Ejercicio_14/src/App.jsx
@@ -1,9 +1,11 @@
+import { Clock } from "./Clock";
 import { Counter } from "./Counter";
 
 export function App() {
   return (
     <div>
       <Counter initialValue={0} increment={1} decrement={1} />
+      <Clock />
     </div>
   );
 }
