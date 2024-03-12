c5fb0836b58d46629af86e7a215f0bc81e10d2e9
Francisco Javier Martinez Rodriguez <javic9165@gmail.com>
2024-03-13 00:09:37 +0100

    Erased comment

--- 28/Ejercicio_28/src/TodoList.jsx
+++ 28/Ejercicio_28/src/TodoList.jsx
@@ -1,15 +1,6 @@
 import { useState } from "react";
 
 export function TodoList() {
-  /*
-    Create a TodoList component that renders a ul tag with a
-    li tag for each item contained in the todos state variable.
-    The todos state variable should be an array of strings.
-    The TodoList component should also contain an input tag 
-    and a button. When the button is clicked, the event 
-    handler should add the value of the input tag to the
-    todos array. */
-
   const [todos, setTodos] = useState([]);
 
   function handleClick(event) {
