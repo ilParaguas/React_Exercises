cec022674930a9130f35dcd9add2126eebca1c19
Francisco Javier Martinez Rodriguez <javic9165@gmail.com>
2024-03-12 15:45:38 +0100

    Ejercicio 26 corrected

--- 26/Ejercicio_26/src/Counter.jsx
+++ 26/Ejercicio_26/src/Counter.jsx
@@ -1,40 +1,47 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 export function Counter({ initialValue = 0, increment = 1, decrement = 1 }) {
   const [counter, setCounter] = useState(initialValue);
   const counterDirection = useRef("");
+  const counterRef = useRef(0);
+  const previousCounter = useRef(0);
+
+  function valueChange() {
+    previousCounter.current = counterRef.current;
+  }
 
   function handleIncrement() {
-    setCounter((x) => x + increment);
-    if (counterDirection.current !== "up") {
-      counterDirection.current = "up";
-      console.log(counterDirection.current);
-    }
+    valueChange();
+    counterRef.current = counterRef.current + increment;
+    setCounter(counterRef.current);
   }
   function handleDecrement() {
-    setCounter((x) => x - decrement);
-    if (counterDirection.current !== "down") {
-      counterDirection.current = "down";
-      console.log(counterDirection.current);
-    }
+    valueChange();
+    counterRef.current = counterRef.current - decrement;
+    setCounter(counterRef.current);
   }
 
   function handleReset() {
-    if (counter < 0) {
-      counterDirection.current = "up";
-      console.log(counterDirection.current);
-    } else if (counter > 0) {
-      counterDirection.current = "down";
-      console.log(counterDirection.current);
-    } else {
-      counterDirection.current = "";
-    }
+    valueChange();
+    counterRef.current = initialValue;
     setCounter(initialValue);
   }
 
   /*To further improve your solution, consider leveraging the useEffect hook to monitor the counter state changes.
   This would allow for a more precise update and logging of the counter's direction relative to its previous state or the initialValue.
   This approach is more aligned with React’s principles of data flow and state management. */
+
+  useEffect(() => {
+    if (previousCounter.current > counterRef.current) {
+      if (counterDirection.current !== "down") {
+        counterDirection.current = "down";
+        console.log(counterDirection.current);
+      }
+    } else if (counterDirection.current !== "up") {
+      counterDirection.current = "up";
+      console.log(counterDirection.current);
+    }
+  }, [counter]);
 
   return (
     <div>
