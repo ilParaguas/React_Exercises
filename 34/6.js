81aafce3c48e8c262ba3ffa14e55eb77e9c78db4
Francisco Javier Martinez Rodriguez <javic9165@gmail.com>
2024-03-13 10:37:59 +0100

    Ejercicio 34 base

---
+++ 34/6.js
@@ -0,0 +1,2 @@
+// Install tailwindcss by following the instructions at: https://tailwindcss.com/docs/guides/create-react-app
+// After installing, play around with the classes by reading the documentation
