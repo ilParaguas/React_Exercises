838b5a7b4f90479f11f2a509f32f3de5fefe11c8
Francisco Javier Martinez Rodriguez <javic9165@gmail.com>
2024-03-18 15:34:21 +0100

    Comentary erase

--- 44/Ejercicio_44/src/useCurrentLocation.js
+++ 44/Ejercicio_44/src/useCurrentLocation.js
@@ -1,12 +1,3 @@
-/**
- Write the useCurrentLocation custom hook, that uses the 
- browser's navigator.geolocation API to get the current 
- location of the user through the getCurrentPosition builtin 
- method.The hook should return the current location as well 
- as a function to get the current location, and the error 
- and loading states.
- */
-
 import { useState } from "react";
 
 export function useCurrentLocation() {
