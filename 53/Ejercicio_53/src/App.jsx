ce87e7e76949bbba9668475a0d7228acd3da720a
Francisco Javier Martinez Rodriguez <javic9165@gmail.com>
2024-03-21 21:26:43 +0100

    Ejercicio 53

--- 53/Ejercicio_53/src/App.jsx
+++ 53/Ejercicio_53/src/App.jsx
@@ -7,14 +7,18 @@ export function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/users/" element={<GithubUserList />}>
+          <Route index element={<p>Add a user and select it</p>} />
           <Route path=":username" element={<ShowGithubUser />} />
         </Route>
-        <Route path="*" element={
-          <div>
-            <p>Not found!</p>
-            <Link to="/users/"> Home </Link>
-          </div>
-        }/>
+        <Route
+          path="*"
+          element={
+            <div>
+              <p>Not found!</p>
+              <Link to="/users/"> Home </Link>
+            </div>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
